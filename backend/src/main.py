94ba5a057d60dcfe833cb34cb31e3680a0a7f5f5
EdwinTJ <edwin16x@gmail.com>
2024-09-27 10:07:04 -0600

    Modularize auth functions

--- backend/src/main.py
+++ backend/src/main.py
@@ -8,63 +8,17 @@ from sqlalchemy.orm import Session
 from database.dependencies import get_db
 
 
-
-# JWT PASSWORD HASHING
-from datetime import datetime, timedelta
-from typing import Optional
-from jose import JWTError, jwt
-from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+#JWT
+from datetime import timedelta
+from auth.auth import hash_password,verify_password,create_access_token,verify_token,get_current_user,oauth2_scheme
 
 # Schemas
 from database.models import User,ChannelInfo
 from schemas.schemas import Token,UserCreate,ChannelCreate,CommentSummarizeRequest,ChannelVideosRequest
 # Environment variables
-from config import SECRET_KEY,ALGORITHM,ACCESS_TOKEN_EXPIRE_MINUTES
+from config import ACCESS_TOKEN_EXPIRE_MINUTES
+from fastapi.security import  OAuth2PasswordRequestForm,OAuth2PasswordBearer
 
-### PWD HASING HANDLING
-
-# Password hashing context
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-# Utility functions for password hashing
-def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
-
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
-
-# Utility functions for JWT token handling
-
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-def verify_token(token: str) -> dict:
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        return payload
-    except JWTError:
-        raise HTTPException(status_code=401, detail="Could not validate credentials")
-
-
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    payload = verify_token(token)
-    user_email = payload.get("email")
-    if user_email is None:
-        raise HTTPException(status_code=401, detail="Invalid token")
-    user = db.query(User).filter(User.email == user_email).first()
-    if user is None:
-        raise HTTPException(status_code=401, detail="User not found")
-    return user
 ###
 app = FastAPI()
 
