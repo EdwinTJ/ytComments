18ecbb2f00c42e15fd597cc182a2deaaeca209af
EdwinTJ <edwin16x@gmail.com>
2024-10-21 23:00:51 -0600

    Cahnge URL

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -1,5 +1,6 @@
 export const getEnvConfig = () => {
   return {
-    VITE_API_URL: import.meta.env.VITE_API_URL || "http://localhost:8000",
+    VITE_API_URL:
+      import.meta.env.VITE_API_URL || "https://ytcomments-hs2i.onrender.com",
   };
 };
